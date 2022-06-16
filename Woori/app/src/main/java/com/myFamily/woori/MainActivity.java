7e2e9e5be5878ed7ddee4be6ead002193e334345
VisionPG <jyseolois@naver.com>
2022-06-16 19:56:57 +0900

    no message

--- Woori/app/src/main/java/com/myFamily/woori/MainActivity.java
+++ Woori/app/src/main/java/com/myFamily/woori/MainActivity.java
@@ -3,23 +3,103 @@ package com.myFamily.woori;
 import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.appcompat.widget.SearchView;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
 import android.widget.Toast;
 
-public class MainActivity extends AppCompatActivity {
+import com.myFamily.woori.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity implements View.OnClickListener {
     SearchView searchView;
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        // 뷰 바인딩 객체 획득
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        // 액티비티 화면 출력
+        setContentView(binding.getRoot());
+
+        binding.anniversarybutton.setOnClickListener(this);
+        binding.memorybutton.setOnClickListener(this);
+        binding.contactbutton.setOnClickListener(this);
+        binding.letterbutton.setOnClickListener(this);
     }
 
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
+    public void onClick(View v) {
+        // androidx.fragment.app.FragmentManager
+        FragmentManager manager = getSupportFragmentManager();
+        // androidx.fragment.app.FragmentTransaction
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (v == binding.anniversarybutton) {
+            AnniversaryFragment anniversaryFragment = new AnniversaryFragment();
+            ft.replace(R.id.main, anniversaryFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (v == binding.memorybutton){
+            MemoryFragment memoryFragment = new MemoryFragment();
+            ft.replace(R.id.main, memoryFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (v == binding.contactbutton){
+            ContactFragment contactFragment = new ContactFragment();
+            ft.replace(R.id.main, contactFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (v == binding.letterbutton){
+            LetterFragment letterFragment = new LetterFragment();
+            ft.replace(R.id.main, letterFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        // androidx.fragment.app.FragmentManager
+        FragmentManager manager = getSupportFragmentManager();
+        // androidx.fragment.app.FragmentTransaction
+        FragmentTransaction ft = manager.beginTransaction();
+
+        if (item.getItemId() == R.id.menu_anniversary) {
+            AnniversaryFragment anniversaryFragment = new AnniversaryFragment();
+            ft.replace(R.id.main, anniversaryFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (item.getItemId() == R.id.menu_main) {
+            // 메인 화면으로 돌아가기
+            Intent intent = new Intent(getApplicationContext(),MainActivity.class);
+            startActivity(intent);
+        } else if (item.getItemId() == R.id.menu_contact) {
+            ContactFragment contactFragment = new ContactFragment();
+            ft.replace(R.id.main, contactFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (item.getItemId() == R.id.menu_letter) {
+            LetterFragment letterFragment = new LetterFragment();
+            ft.replace(R.id.main, letterFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        } else if (item.getItemId() == R.id.menu_memory) {
+            MemoryFragment memoryFragment = new MemoryFragment();
+            ft.replace(R.id.main, memoryFragment);
+            ft.addToBackStack(null);
+            ft.commit();
+        }
+        return super.onOptionsItemSelected(item);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu (Menu menu){
         getMenuInflater().inflate(R.menu.menu_main, menu);
         MenuItem menuItem = menu.findItem(R.id.menu_search);
         searchView = (SearchView) menuItem.getActionView();
@@ -43,26 +123,5 @@ public class MainActivity extends AppCompatActivity {
             return false;
         }
     };
-
-    @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.menu_anniversary) {
-            Toast t = Toast.makeText(this, "anniversary clicked", Toast.LENGTH_SHORT);
-            t.show();
-        } else if (item.getItemId() == R.id.menu_main) {
-            Toast t = Toast.makeText(this, "main clicked", Toast.LENGTH_SHORT);
-            t.show();
-        } else if (item.getItemId() == R.id.menu_contact) {
-            Toast t = Toast.makeText(this, "contact clicked", Toast.LENGTH_SHORT);
-            t.show();
-        } else if (item.getItemId() == R.id.menu_letter) {
-            Toast t = Toast.makeText(this, "letter clicked", Toast.LENGTH_SHORT);
-            t.show();
-        } else if (item.getItemId() == R.id.menu_memory) {
-            Toast t = Toast.makeText(this, "memory clicked", Toast.LENGTH_SHORT);
-            t.show();
-        }
-        return super.onOptionsItemSelected(item);
-    }
 
 }
---
+++ Woori/app/src/main/java/com/myFamily/woori/MemoryFragment.java
@@ -0,0 +1,19 @@
+package com.myFamily.woori;
+
+import androidx.fragment.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import com.myFamily.woori.databinding.FragmentMemoryBinding;
+
+public class MemoryFragment extends Fragment {
+
+    @Override
+    public View onCreateView(LayoutInflater inflator, ViewGroup container, Bundle savedInstanceState) {
+        FragmentMemoryBinding binding = FragmentMemoryBinding.inflate(inflator, container, false);
+        return binding.getRoot();
+    }
+
+}
