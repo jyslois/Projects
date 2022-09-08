06abd0cee4149e199854685561ff2e5352e7f57c
VisionPG <jyseolois@naver.com>
2022-09-08 17:42:39 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/MainActivity.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/MainActivity.java
@@ -1,14 +1,22 @@
 package com.android.mymindnotes;
 
 import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
 
+import com.android.mymindnotes.databinding.ActivityMainBinding;
+import com.bumptech.glide.Glide;
+
 public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        // gif 이미지를 이미지뷰에 띄우기
+        Glide.with(this).load(R.drawable.sky).into(binding.background);
+
     }
 }
