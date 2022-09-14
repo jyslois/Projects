2548de774f2841a0d6e9ea869f0f89855003630a
VisionPG <jyseolois@naver.com>
2022-09-14 17:33:17 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/New_Situation.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/New_Situation.java
@@ -1,5 +1,6 @@
 package com.android.mymindnotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
@@ -14,5 +15,18 @@ public class New_Situation extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         binding = ActivityNewSituationBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
+
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.tips);
+        builder.setTitle("상황 작성 Tips");
+        builder.setMessage(R.string.situationTips);
+        builder.setPositiveButton("확인", null);
+
+        binding.RecordSituationTips.setOnClickListener(view -> {
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
+        });
+
     }
 }
