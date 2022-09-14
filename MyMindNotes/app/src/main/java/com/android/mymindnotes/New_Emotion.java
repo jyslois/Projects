8460fdd755f29d00e5511481c7314dac9eff139d
VisionPG <jyseolois@naver.com>
2022-09-14 17:19:00 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/New_Emotion.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/New_Emotion.java
@@ -1,9 +1,16 @@
 package com.android.mymindnotes;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.fragment.app.DialogFragment;
 
+import android.app.Dialog;
+import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
+import android.widget.ImageView;
+import android.widget.Toast;
 
 import com.android.mymindnotes.databinding.ActivityNewEmotionBinding;
 
@@ -19,6 +26,22 @@ public class New_Emotion extends AppCompatActivity {
         binding.RecordEmotionHelpButton.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), EmotionInstructions.class);
             startActivity(intent);
+        });
+
+        binding.RecordNextButton.setOnClickListener(view -> {
+            Intent intent = new Intent(getApplicationContext(), New_Situation.class);
+            startActivity(intent);
+        });
+
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setIcon(R.drawable.tips);
+        builder.setTitle("감정 작성 Tips");
+        builder.setMessage(R.string.emotionTips);
+        builder.setPositiveButton("확인", null);
+
+        binding.RecordTips.setOnClickListener(view -> {
+            AlertDialog alertDialog = builder.create();
+            alertDialog.show();
         });
 
     }
