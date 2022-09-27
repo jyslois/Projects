236e90f814e699d032a6ede225e29dfabd4f5e82
VisionPG <jyseolois@naver.com>
2022-09-28 00:36:13 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/Diary.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/Diary.java
@@ -48,28 +48,16 @@ public class Diary extends AppCompatActivity {
         inflater.inflate(R.menu.menu_main, menu);
 
         // 액션바 [감정 설명서] 아이콘 사이즈 조절
-        Bitmap emotionIcon = BitmapFactory.decodeResource(getResources(), R.drawable.emotioninstructionicon); //Converting drawable into bitmap
+        Bitmap emotionIcon = BitmapFactory.decodeResource(getResources(), R.drawable.emotionicon); //Converting drawable into bitmap
         Bitmap new_emotionIcon = resizeBitmapImageFn(emotionIcon, 70); //resizing the bitmap
         Drawable d = new BitmapDrawable(getResources(),new_emotionIcon); //Converting bitmap into drawable
         menu.getItem(0).setIcon(d); //choose the item number you want to set
 
         // 액션바 [나의 마음 일지] 아이콘 사이즈 조절
-        Bitmap DiaryIcon = BitmapFactory.decodeResource(getResources(), R.drawable.diaryicon); //Converting drawable into bitmap
+        Bitmap DiaryIcon = BitmapFactory.decodeResource(getResources(), R.drawable.diary_icon); //Converting drawable into bitmap
         Bitmap new_DiaryIcon = resizeBitmapImageFn(DiaryIcon, 65); //resizing the bitmap
         Drawable f = new BitmapDrawable(getResources(),new_DiaryIcon); //Converting bitmap into drawable
         menu.getItem(1).setIcon(f); //choose the item number you want to set
-
-        // 액션바 [감정 설명서] 아이콘 하얀색 틴트
-        Drawable drawable = menu.findItem(R.id.EmotionInstructionsIcon).getIcon();
-        drawable = DrawableCompat.wrap(drawable);
-        DrawableCompat.setTint(drawable, ContextCompat.getColor(this,R.color.white));
-        menu.findItem(R.id.EmotionInstructionsIcon).setIcon(drawable);
-
-        // 액션바 [나의 마음 일지] 아이콘 하얀색 틴트
-        Drawable drawable2 = menu.findItem(R.id.DiaryIcon).getIcon();
-        drawable2 = DrawableCompat.wrap(drawable2);
-        DrawableCompat.setTint(drawable2, ContextCompat.getColor(this,R.color.white));
-        menu.findItem(R.id.DiaryIcon).setIcon(drawable2);
 
         return true;
     }
@@ -105,7 +93,7 @@ public class Diary extends AppCompatActivity {
     // 엑션바 메뉴를 클릭했을 때 이벤트
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.EmotionInstructionsIcon) {
+        if (item.getItemId() == R.id.emotionicon) {
             Intent intent = new Intent(getApplicationContext(), EmotionInstructions.class);
             startActivity(intent);
         }
--- MyMindNotes/app/src/main/java/com/android/mymindnotes/MainPage.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/MainPage.java
@@ -23,6 +23,7 @@ import androidx.core.content.ContextCompat;
 import androidx.core.graphics.drawable.DrawableCompat;
 
 import com.android.mymindnotes.databinding.ActivityMainPageBinding;
+import com.bumptech.glide.Glide;
 
 public class MainPage extends AppCompatActivity {
     ActivityMainPageBinding binding;
@@ -34,9 +35,8 @@ public class MainPage extends AppCompatActivity {
         binding = ActivityMainPageBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
-        // 닉네임 설정
-        nickName = getSharedPreferences("nickName",Activity.MODE_PRIVATE);
-        binding.UserNickName.setText(nickName.getString("nickName", "") + " 님.");
+        // gif 이미지를 이미지뷰에 띄우기
+        Glide.with(this).load(R.drawable.mainbackground).into(binding.background);
 
 
         // 커스텀한 toolbar 적용시키기
@@ -44,10 +44,16 @@ public class MainPage extends AppCompatActivity {
         setSupportActionBar(toolbar); // 이 액티비티에서 툴바 사용
         getSupportActionBar().setDisplayShowTitleEnabled(false); // 타이틀 안 보이게 하기
 
-        binding.RecordMindButton.setOnClickListener(view -> {
+        binding.addRecordButton.setOnClickListener(view -> {
             Intent intent = new Intent(getApplicationContext(), RecordMindChoice.class);
             startActivity(intent);
         });
+
+        // 닉네임 설정
+        nickName = getSharedPreferences("nickName", Activity.MODE_PRIVATE);
+        String nick = nickName.getString("nickName", "");
+        binding.mainpagetext.setText("오늘 하루도 고생했어요, " + nick + " 님.");
+
 
     }
 
@@ -75,68 +81,17 @@ public class MainPage extends AppCompatActivity {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.menu_main, menu);
 
-        // 액션바 [감정 설명서] 아이콘 사이즈 조절
-        Bitmap emotionIcon = BitmapFactory.decodeResource(getResources(), R.drawable.emotioninstructionicon); //Converting drawable into bitmap
-        Bitmap new_emotionIcon = resizeBitmapImageFn(emotionIcon, 70); //resizing the bitmap
-        Drawable d = new BitmapDrawable(getResources(),new_emotionIcon); //Converting bitmap into drawable
-        menu.getItem(0).setIcon(d); //choose the item number you want to set
-
-        // 액션바 [나의 마음 일지] 아이콘 사이즈 조절
-        Bitmap DiaryIcon = BitmapFactory.decodeResource(getResources(), R.drawable.diaryicon); //Converting drawable into bitmap
-        Bitmap new_DiaryIcon = resizeBitmapImageFn(DiaryIcon, 65); //resizing the bitmap
-        Drawable f = new BitmapDrawable(getResources(),new_DiaryIcon); //Converting bitmap into drawable
-        menu.getItem(1).setIcon(f); //choose the item number you want to set
-
-        // 액션바 [감정 설명서] 아이콘 하얀색 틴트
-        Drawable drawable = menu.findItem(R.id.EmotionInstructionsIcon).getIcon();
-        drawable = DrawableCompat.wrap(drawable);
-        DrawableCompat.setTint(drawable, ContextCompat.getColor(this,R.color.white));
-        menu.findItem(R.id.EmotionInstructionsIcon).setIcon(drawable);
-
-        // 액션바 [나의 마음 일지] 아이콘 하얀색 틴트
-        Drawable drawable2 = menu.findItem(R.id.DiaryIcon).getIcon();
-        drawable2 = DrawableCompat.wrap(drawable2);
-        DrawableCompat.setTint(drawable2, ContextCompat.getColor(this,R.color.white));
-        menu.findItem(R.id.DiaryIcon).setIcon(drawable2);
-
         return true;
-    }
-
-    // 엑션바 메뉴 아이콘 사이즈 조절을 위한 메서드
-    private Bitmap resizeBitmapImageFn(
-            Bitmap bmpSource, int maxResolution){
-        int iWidth = bmpSource.getWidth();
-        int iHeight = bmpSource.getHeight();
-        int newWidth = iWidth ;
-        int newHeight = iHeight ;
-        float rate = 0.0f;
-
-        if(iWidth > iHeight ){
-            if(maxResolution < iWidth ){
-                rate = maxResolution / (float) iWidth ;
-                newHeight = (int) (iHeight * rate);
-                newWidth = maxResolution;
-            }
-        }else{
-            if(maxResolution < iHeight ){
-                rate = maxResolution / (float) iHeight ;
-                newWidth = (int) (iWidth * rate);
-                newHeight = maxResolution;
-            }
-        }
-
-        return Bitmap.createScaledBitmap(
-                bmpSource, newWidth, newHeight, true);
     }
 
 
     // 엑션바 메뉴를 클릭했을 때 이벤트
     @Override
     public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        if (item.getItemId() == R.id.EmotionInstructionsIcon) {
+        if (item.getItemId() == R.id.emotionicon) {
             Intent intent = new Intent(getApplicationContext(), EmotionInstructions.class);
             startActivity(intent);
-        } else if (item.getItemId() == R.id.DiaryIcon) {
+        } else if (item.getItemId() == R.id.diaryicon) {
             Intent intent = new Intent(getApplicationContext(), Diary.class);
             startActivity(intent);
         }
