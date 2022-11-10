002b743226aa9c11c4f2102cf55cfcc99b30b9a3
VisionPG <jyseolois@naver.com>
2022-11-10 17:03:50 +0900

    no message

--- MyMindNotes 기획/spring/src/main/java/com/android/mymindnotes/spring/controller/UserInfoController.java
+++ MyMindNotes 기획/spring/src/main/java/com/android/mymindnotes/spring/controller/UserInfoController.java
@@ -3,11 +3,15 @@ package com.android.mymindnotes.spring.controller;
 import com.android.mymindnotes.spring.mapper.UserInfoMapper;
 import com.android.mymindnotes.spring.model.UserInfo;
 import org.springframework.http.HttpStatus;
+import org.springframework.validation.Errors;
+import org.springframework.validation.FieldError;
 import org.springframework.web.bind.annotation.*;
 
+import javax.validation.Valid;
 import java.util.HashMap;
 import java.util.Map;
 
+// 회원 관련 API
 @RestController
 public class UserInfoController {
     // Mapper 사용하기: API 호출
@@ -22,35 +26,63 @@ public class UserInfoController {
         this.mapper = mapper;
     }
 
-    // 회원 관련 API
+    // 이메일 중복 체크
+    @GetMapping("/api/member/checkEmail/{email}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Map<String, Object> checkEmail(@PathVariable("email") String email) {
+        Map<String, Object> result = new HashMap<>();
+        // 회원 정보 조회
+        UserInfo userinfo = mapper.getUserInfoFromEmail(email);
+        // 존재하는 회원이라면
+        if (userinfo != null) {
+            result.put("code", 1001);
+            result.put("msg", "이미 가입된 이메일입니다");
+            // 존재하지 않는 회원이라면
+        } else {
+            result.put("code", 1000);
+            result.put("msg", "사용 가능한 이메일입니다");
+        }
+        return result;
+    }
+
+    // 닉네임 중복 체크
+    @GetMapping("/api/member/checkNickname/{nickname}")
+    @ResponseStatus(value = HttpStatus.OK)
+    public Map<String, Object> checkNickname(@PathVariable("nickname") String nickname) {
+        Map<String, Object> result = new HashMap<>();
+        // 회원 정보 조회
+        UserInfo userinfo = mapper.getUserInfoFromNickname(nickname);
+        // 존재하는 회원이라면
+        if (userinfo != null) {
+            result.put("code", 1003);
+            result.put("msg", "이미 사용 중인 닉네임입니다");
+            // 존재하지 않는 회원이라면
+        } else {
+            result.put("code", 1002);
+            result.put("msg", "사용 가능한 닉네임입니다");
+        }
+        return result;
+    }
+
     // 회원가입
     @PostMapping("/api/member/add")
     @ResponseStatus(value = HttpStatus.OK)
-    public void addUser(@RequestBody UserInfo userinfo) {
-        // mapper의 api 호출 - api에 매핑된 SQL문 실행
-        mapper.insertUser(userinfo.getEmail(), userinfo.getNickname(), userinfo.getPassword(), userinfo.getBirthyear());
-    }
-
-    // 회원 정보 가져오기(조회)
-    // 이 mapper에 정의된 getUserInfo라는 API를 호출하게 될 것이고,
-    // Path로 전달된 email이 파라미터로 전달될 것이다.
-    // api가 호출되면, UserInfoMapper에서 해당 api와 매핑된 sql문이 수행되면서
-    // 해당 조건을 갖는 UserInfo의 정보가 자바의 객체로 반환이 되고, 이것을 json형태로 전달되는 것이다.
-    @GetMapping("/api/member/get/{email}")
-    @ResponseStatus(value = HttpStatus.OK)
-    public Map<String, Object> getUserInfo(@PathVariable("email") String email) {
+    public Map<String, Object> addUser(@RequestBody @Valid UserInfo userinfo, Errors errors) {
         Map<String, Object> result = new HashMap<>();
 
-        UserInfo userinfo = mapper.getUserInfo(email);
-
-        // 존재하는 회원이라면
-        if (userinfo != null) {
-            result.put("code", 1000);
-            result.put("회원정보", userinfo);
-        // 존재하지 않는 회원이라면
+        // 유효성 통과 못한 필드와 메시지를 핸들링
+        if (errors.hasErrors()) {
+            // 유효성 검사에 실패한 필드 목록을 가져온다: errors.getFieldErrors()
+            for (FieldError error : errors.getFieldErrors()) {
+                // 유효성 검사에 실패한 필드명을 가져온다. error.getField()
+                // 유효성 검사에 실패한 필드에 정의된 메시지를 가져온다. error.getDefaultMessage()
+                result.put("code", 2001);
+                result.put(error.getField(), error.getDefaultMessage());
+            }
         } else {
-            result.put("code", 1001);
-            result.put("msg", "존재하지 않는 회원입니다.");
+            mapper.insertUser(userinfo.getEmail(), userinfo.getNickname(), userinfo.getPassword(), userinfo.getBirthyear());
+            result.put("code", 2000);
+            result.put("msg", "회원 가입 성공! 환영합니다");
         }
 
         return result;
@@ -59,15 +91,36 @@ public class UserInfoController {
     // 회원정보 수정
     @PutMapping("/api/member/update/{email}")
     @ResponseStatus(value = HttpStatus.OK)
-    public void updateUserInfo(@PathVariable("email") String email, @RequestParam("nickname") String nickname, @RequestParam("password") String password, @RequestParam("birthyear") int birthyear) {
-         mapper.updateUserInfo(email, nickname, password, birthyear);
+    public Map<String, Object> updateUserInfo(@PathVariable("email") String email, @RequestBody @Valid UserInfo userinfo, Errors errors) {
+        Map<String, Object> result = new HashMap<>();
+        // 유효성 통과 못한 필드와 메시지를 핸들링
+        if (errors.hasErrors()) {
+            // 유효성 검사에 실패한 필드 목록을 가져온다: errors.getFieldErrors()
+            for (FieldError error : errors.getFieldErrors()) {
+                // 유효성 검사에 실패한 필드명을 가져온다. error.getField()
+                // 유효성 검사에 실패한 필드에 정의된 메시지를 가져온다. error.getDefaultMessage()
+                result.put("code", 3001);
+                result.put(error.getField(), error.getDefaultMessage());
+            }
+        } else {
+            mapper.updateUserInfo(email, userinfo.getNickname(), userinfo.getPassword());
+            result.put("code", 3000);
+            result.put("msg", "회원 정보가 수정되었습니다");
+        }
+
+        return result;
     }
 
     // 회원탈퇴
     @DeleteMapping("/api/member/delete/{email}")
     @ResponseStatus(value = HttpStatus.OK)
     public void deleteUser(@PathVariable("email") String email) {
+        Map<String, Object> result = new HashMap<>();
         mapper.deleteUser(email);
+        result.put("code", 4000);
+        result.put("msg", "회원 탈퇴 완료");
     }
+
+    // 로그인
 
 }
--- MyMindNotes 기획/spring/src/main/java/com/android/mymindnotes/spring/model/UserInfo.java
+++ MyMindNotes 기획/spring/src/main/java/com/android/mymindnotes/spring/model/UserInfo.java
@@ -1,9 +1,20 @@
 package com.android.mymindnotes.spring.model;
 
+import javax.validation.constraints.*;
+
 public class UserInfo {
+    @NotBlank(message = "이메일을 입력해 주세요.")
+    @Email(message = "올바른 이메일 형식으로 입력해 주세요.")
     private String email;
+    @NotBlank(message = "닉네임을 입력해 주세요.")
+    @Pattern(regexp = "^[ㄱ-ㅎ가-힣a-z0-9-_]{2,10}$", message = "닉네임은 특수문자를 제외한 2~10자리여야 합니다.")
     private String nickname;
+    @NotBlank(message = "비밀번호를 입력해 주세요.")
+    @Pattern(regexp = "(?=.*[0-9])(?=.*[a-zA-Z]).{6,20}", message = "비밀번호는 영문 대,소문자와 숫자가 적어도 1개 이상씩 포함된 6자 ~ 20자의 비밀번호여야 합니다.")
     private String password;
+    @NotNull(message = "태어난 생년을 입력해 주세요.") // NotBlank는 String 타입에, NotNull은 int 타입에
+    @Max(value = 2155, message = "생년은 1901~2155 사이여야 합니다.")
+    @Min(value = 1901, message = "생년은 1901~2155 사이여야 합니다.")
     private int birthyear;
 
     public UserInfo(String email, String nickname, String password, int birthyear) {
