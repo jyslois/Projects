27c8a4a579ec9c6fb0633f9d86d39fd5b41182f6
VisionPG <jyseolois@naver.com>
2022-12-09 13:27:18 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/accountInformation.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/accountInformation.java
@@ -9,6 +9,7 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.graphics.Point;
 import android.os.Bundle;
+import android.text.method.ScrollingMovementMethod;
 import android.view.Display;
 import android.widget.TextView;
 import android.widget.Toast;
@@ -76,7 +77,7 @@ public class AccountInformation extends AppCompatActivity {
         binding.emailText.setTextSize((float) (standardSize_X / 23));
         binding.nickNameText.setTextSize((float) (standardSize_X / 23));
         binding.birthyearText.setTextSize((float) (standardSize_X / 23));
-        binding.email.setTextSize((float) (standardSize_X / 24));
+        binding.emailtype.setTextSize((float) (standardSize_X / 24));
         binding.nickname.setTextSize((float) (standardSize_X / 24));
         binding.birthyear.setTextSize((float) (standardSize_X / 24));
         binding.changeNicknameButton.setTextSize((float) (standardSize_X / 24));
@@ -146,7 +147,7 @@ public class AccountInformation extends AppCompatActivity {
                 @Override
                 public void onResponse(Call<Map<String, Object>> call, Response<Map<String, Object>> response) {
                     // 이메일, 닉네임, 생년 세팅
-                    binding.email.setText(String.valueOf(response.body().get("email")));
+                    binding.emailtype.setText(String.valueOf(response.body().get("email")));
                     binding.nickname.setText((String.valueOf(response.body().get("nickname"))));
                     int birthyear = (int) Double.parseDouble(String.valueOf((response.body().get("birthyear"))));
                     binding.birthyear.setText(String.valueOf(birthyear));
