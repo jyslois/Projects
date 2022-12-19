6351f6b70ee9606ec0602476d1678b8371559b4b
VisionPG <jyseolois@naver.com>
2022-12-19 10:46:11 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/AlarmBootReceiver.java
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/AlarmBootReceiver.java
@@ -18,12 +18,14 @@ public class AlarmBootReceiver extends BroadcastReceiver {
 
         // 부팅 시 알람 설정을 위한 코드
         if (intent.getAction().equals("android.intent.action.BOOT_COMPLETED")) {
+            Log.e("MyChecker:BootCheck", "BootCheck : " + "여기까지 무사히 들어왔다.");
             // Set the alarm here.
             if (timeSave.getLong("time", 0) != 0) {
                 // 저장된 알람이 있을 경우에만 reset alarm
                 Calendar calendar = Calendar.getInstance();
                 calendar.setTimeInMillis(timeSave.getLong("time", 0));
                 AlarmSetting.setAlarm(calendar, context);
+                Log.e("MyChecker:BootCheck", "BootCheck : " + calendar.getTime());
             }
         }
     }
