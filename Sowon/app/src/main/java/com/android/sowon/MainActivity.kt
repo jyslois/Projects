dfdc00a3ad7f843270e1e3e797731d5a37e215a8
VisionPG <jyseolois@naver.com>
2023-01-30 19:37:17 +0900

    no message

--- Sowon/app/src/main/java/com/android/sowon/MainActivity.kt
+++ Sowon/app/src/main/java/com/android/sowon/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.android.sowon
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.android.sowon.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding : ActivityMainBinding
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        // 임시
+        binding.button.setOnClickListener {
+            // 메인 페이지로 이동
+            val intent: Intent = Intent(this, MainPage::class.java)
+            // 인텐트를 시스템에 전달(시작)
+            startActivity(intent)
+        }
+
     }
 }
