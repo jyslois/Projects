4b2798bc2a744f73dbc85967b9289320c5376c19
VisionPG <jyseolois@naver.com>
2023-03-30 16:00:41 +0900

    no message

--- MyMindNotes/app/src/main/java/com/android/mymindnotes/data/repositoryImpl/MemberRepositoryImpl.kt
+++ MyMindNotes/app/src/main/java/com/android/mymindnotes/data/repositoryImpl/MemberRepositoryImpl.kt
@@ -19,6 +19,22 @@ class MemberRepositoryImpl @Inject constructor(
     @MainDispatcherCoroutineScope private val mainDispatcherCoroutineScope: CoroutineScope
 ) : MemberRepository {
 
+    // 로그인
+    override suspend fun login(email: String, password: String): Flow<Map<String, Object>> = memberDataSource.loginResultFlow(email, password)
+
+    // 아이디, 비밀번호 중복 체크
+    // 이메일 중복 체크
+    override suspend fun checkEmail(emailInput: String): Flow<Map<String, Object>> = memberDataSource.emailCheckFlow(emailInput)
+
+    // 닉네임 중복 체크
+    override suspend fun checkNickName(nickNameInput: String): Flow<Map<String, Object>> = memberDataSource.nickNameCheckFlow(nickNameInput)
+
+    // 회원가입
+    override suspend fun join(email: String, nickname: String, password: String, birthyear: Int): Flow<Map<String, Object>> = memberDataSource.joinResultFlow(email, nickname, password, birthyear)
+
+    // 회원탈퇴
+    override suspend fun deleteUser(): Flow<Map<String, Object>> = memberDataSource.deleteUserResultFlow
+
     // 회원 정보 가져오기
     // 받은 회원 정보 저장하는 플로우
     private val _userInfo = MutableStateFlow<Map<String, Object>>(emptyMap())
@@ -26,9 +42,14 @@ class MemberRepositoryImpl @Inject constructor(
 
     // (서버) 회원 정보 가져오기
     override suspend fun getUserInfo() {
-       memberDataSource.getUserInfo()
+        memberDataSource.getUserInfo()
         Log.e("UserInfoCheck", "Repository - 함수콜")
     }
+    
+    // 에러
+    // 에러 메시지
+    private val _error = MutableSharedFlow<Boolean>(replay = 1)
+    override val error = _error.asSharedFlow()
 
     init {
         mainDispatcherCoroutineScope.launch {
@@ -48,26 +69,5 @@ class MemberRepositoryImpl @Inject constructor(
             }
         }
     }
-
-    // 로그인
-    override suspend fun login(email: String, password: String): Flow<Map<String, Object>> = memberDataSource.loginResultFlow(email, password)
-
-    // 아이디, 비밀번호 중복 체크
-    // 이메일 중복 체크
-    override suspend fun checkEmail(emailInput: String): Flow<Map<String, Object>> = memberDataSource.emailCheckFlow(emailInput)
-
-    // 닉네임 중복 체크
-    override suspend fun checkNickName(nickNameInput: String): Flow<Map<String, Object>> = memberDataSource.nickNameCheckFlow(nickNameInput)
-
-    // 회원가입
-    override suspend fun join(email: String, nickname: String, password: String, birthyear: Int): Flow<Map<String, Object>> = memberDataSource.joinResultFlow(email, nickname, password, birthyear)
-
-    // 회원탈퇴
-    override suspend fun deleteUser(): Flow<Map<String, Object>> = memberDataSource.deleteUserResultFlow
-
-    // 에러
-    // 에러 메시지
-    private val _error = MutableSharedFlow<Boolean>(replay = 1)
-    override val error = _error.asSharedFlow()
 
 }
