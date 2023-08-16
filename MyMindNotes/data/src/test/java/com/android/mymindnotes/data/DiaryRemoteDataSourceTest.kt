a9686d0daa574c4ddbbea2c09ab0763fbfde5e1e
VisionPG <jyseolois@naver.com>
2023-08-17 00:03:31 +0900

    Unit test code for DiaryRemoteDataSource

--- MyMindNotes/data/src/test/java/com/android/mymindnotes/data/DiaryRemoteDataSourceTest.kt
+++ MyMindNotes/data/src/test/java/com/android/mymindnotes/data/DiaryRemoteDataSourceTest.kt
@@ -1,10 +1,11 @@
-package com.android.mymindnotes.data.dataSources
+package com.android.mymindnotes.data
 
 import com.android.mymindnotes.core.dto.DeleteDiaryResponse
 import com.android.mymindnotes.core.dto.Diary
 import com.android.mymindnotes.core.dto.DiaryEdit
 import com.android.mymindnotes.core.dto.GetDiaryListResponse
 import com.android.mymindnotes.core.dto.UpdateDiaryResponse
+import com.android.mymindnotes.data.dataSources.DiaryRemoteDataSource
 import com.android.mymindnotes.data.retrofit.api.diary.DeleteDiaryApi
 import com.android.mymindnotes.data.retrofit.api.diary.UpdateDiaryApi
 import com.android.mymindnotes.data.retrofit.api.user.GetDiaryListApi
