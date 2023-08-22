e7151ed9b1dc140e4efc2820715dc62d97d7e9f5
VisionPG <jyseolois@naver.com>
2023-08-22 19:16:09 +0900

    unit test code for GetUserInfoUseCase

--- MyMindNotes/domain/src/main/java/com/android/mymindnotes/domain/usecases/userInfoRemote/GetUserInfoUseCase.kt
+++ MyMindNotes/domain/src/main/java/com/android/mymindnotes/domain/usecases/userInfoRemote/GetUserInfoUseCase.kt
@@ -2,26 +2,26 @@ package com.android.mymindnotes.domain.usecases.userInfoRemote
 
 import com.android.mymindnotes.data.repositoryInterfaces.MemberRepository
 import kotlinx.coroutines.flow.Flow
-import kotlinx.coroutines.flow.catch
-import kotlinx.coroutines.flow.map
+import kotlinx.coroutines.flow.first
+import kotlinx.coroutines.flow.flow
 import javax.inject.Inject
 
 class GetUserInfoUseCase @Inject constructor(
     private val memberRepository: MemberRepository
 ) {
-
-
-//    // 회원 정보 불러오기
-//    suspend fun getUserInfo(): Flow<Map<String, Object>> = memberRemoteRepository.getUserInfo()
-
     suspend operator fun invoke(): Flow<Result<UserInfo>> {
-        return memberRepository.getUserInfo().map { response ->
-            val nickname = response.nickname
-            val email = response.email
-            val birthyear = response.birthyear
-            Result.success(UserInfo(nickname, email, birthyear))
-        }.catch {
-            emit(Result.failure(RuntimeException("서버와의 통신에 실패했습니다. 인터넷 연결을 확인해 주세요.")))
+        // UserInfo 객체를 담고 있는 성공의 Result 또는 예외를 담고 있는 실패의 Result를 가진 Flow를 비동기적으로 반환한다
+        return flow {
+            try {
+                val response = memberRepository.getUserInfo().first() // GetUserInfoResponse를 가져온다
+                val nickname = response.nickname
+                val email = response.email
+                val birthyear = response.birthyear
+
+                emit(Result.success(UserInfo(nickname, email, birthyear))) // UserInfo 객체를 담고 있는 성공한 Result 객체를 flow에 emit한다
+            } catch (e: Exception) {
+                emit(Result.failure<UserInfo>(RuntimeException("서버와의 통신에 실패했습니다. 인터넷 연결을 확인해 주세요."))) // RuntimeException 예외를 담고 있는 실패한 Result 객체를 flow에 emit한다
+            }
         }
     }
 }
--- MyMindNotes/domain/src/test/java/com/android/mymindnotes/domain/GetUserInfoUseCaseTest.kt
+++ MyMindNotes/domain/src/test/java/com/android/mymindnotes/domain/GetUserInfoUseCaseTest.kt
@@ -3,7 +3,9 @@ package com.android.mymindnotes.domain
 import com.android.mymindnotes.core.dto.GetUserInfoResponse
 import com.android.mymindnotes.data.repositoryInterfaces.MemberRepository
 import com.android.mymindnotes.domain.usecases.userInfoRemote.GetUserInfoUseCase
+import io.mockk.clearMocks
 import io.mockk.coEvery
+import io.mockk.coVerify
 import io.mockk.mockk
 import kotlinx.coroutines.ExperimentalCoroutinesApi
 import kotlinx.coroutines.flow.flowOf
@@ -29,20 +31,21 @@ class GetUserInfoUseCaseTest {
 
     @After
     fun tearDown() {
-
+        clearMocks(mockMemberRepository)
     }
 
+    // 테스트의 목적: GetUserInfoUseCase가 정상적인 응답을 받았을 때 올바르게 동작하는지 테스트
     @Test
-    fun invoke_NormalResponse() = runTest(testDispatcher) {
+    fun invoke_ReturnsNormalResponse() = runTest(testDispatcher) { // invoke_NormalResponse 함수는 runTest를 사용하여 테스트 코루틴을 동기적으로 실행하고 그 결과 (성공 또는 실패)를 리턴한다
         // Given
         val nickname = "로이스"
         val email = "seolois@hotmail.com"
         val birthyear = 1991
         val expectedResponse = GetUserInfoResponse(birthyear = birthyear, email = email, nickname = nickname)
-        coEvery { mockMemberRepository.getUserInfo() } returns flowOf(expectedResponse)
+        coEvery { mockMemberRepository.getUserInfo() } returns flowOf(expectedResponse) // mock 객체가 getUserInfo()를 호출할 때 expectedResponse를 담은 flow를 반환하도록 설정
 
         // When
-        val actualResponseFlow = getUserInfoUseCase.invoke()
+        val actualResponseFlow = getUserInfoUseCase()
 
         // Then
         actualResponseFlow.collect {
@@ -53,5 +56,27 @@ class GetUserInfoUseCaseTest {
             assertEquals(email, userInfo.email)
             assertEquals(nickname, userInfo.nickname)
         }
+        coVerify { mockMemberRepository.getUserInfo() } // mock 객체가 getUserInfo()를 한 번 호출했는지 확인
+    }
+
+    // getUserInfo() 호출이 RuntimeException을 발생시킬 때 GetUserInfoUseCase가 예상대로 동작하는지 테스트
+    @Test
+    fun invoke_ThrowsRuntimeException() = runTest {
+        // Given
+        val errorMessage = "서버와의 통신에 실패했습니다. 인터넷 연결을 확인해 주세요."
+        coEvery { mockMemberRepository.getUserInfo() } throws RuntimeException(errorMessage)
+
+        // When
+        val actualResponseFlow = getUserInfoUseCase()
+
+        // Then
+        actualResponseFlow.collect {
+            assertTrue(it.isFailure) // 실패한 결과인지 확인
+            val exception = it.exceptionOrNull()
+            assertNotNull(exception) // 예외가 null이 아닌지 확인
+            assertTrue(exception is RuntimeException) // 발생한 예외가 RuntimeException인지 확인
+            assertEquals(errorMessage, exception.message) // 예외 메시지가 기대한 메시지와 동일한지 확인
+        }
+        coVerify { mockMemberRepository.getUserInfo() } // mock 객체가 getUserInfo()를 한 번 호출했는지 확인
     }
 }
