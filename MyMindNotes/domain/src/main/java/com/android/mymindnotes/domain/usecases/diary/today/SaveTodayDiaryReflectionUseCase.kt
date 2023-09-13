a47b18dcdaf8ba76be55122e6f66e186a6631d29
VisionPG <jyseolois@naver.com>
2023-09-13 21:21:11 +0900

    unit test for SaveTodayDirayReflectionUseCase

--- MyMindNotes/domain/src/main/java/com/android/mymindnotes/domain/usecases/diary/today/SaveTodayDiaryReflectionUseCase.kt
+++ MyMindNotes/domain/src/main/java/com/android/mymindnotes/domain/usecases/diary/today/SaveTodayDiaryReflectionUseCase.kt
@@ -6,11 +6,6 @@ import javax.inject.Inject
 class SaveTodayDiaryReflectionUseCase @Inject constructor(
     private val repository: TodayDiaryRepository
 ) {
-
-//    suspend fun saveReflection(reflection: String?) {
-//        repository.saveReflection(reflection)
-//    }
-
     suspend operator fun invoke(reflection: String?) {
         repository.saveReflection(reflection)
     }
